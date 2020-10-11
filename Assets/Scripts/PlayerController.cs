911b4eb25143046a05f3d2542eec48e23a37bcb4
Layton Vincent-Stewart 10005924 <41661784+Guitarkid00@users.noreply.github.com>
2020-10-12 05:26:16 +1300

    First upload, contains a few days
    First upload of rougelike project. Contains what I have worked on the past 3 days. Moving the player, ability to shoot, moving enemy, enemy can shoot, healths systems for player and enemy, and static dangers that harm the player

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,138 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController instance;
+
+    public float moveSpeed;
+    private Vector2 moveInput;
+
+    public Rigidbody2D theRB;
+
+    private Camera theCam;
+    public Transform gunArm;
+
+    public Animator Anim;
+
+    public GameObject bulletToFire;
+    public Transform firePoint;
+
+    public float timeBetweenShots;
+    private float shotCounter;
+
+    public SpriteRenderer bodySR;
+
+    private float activeMoveSpeed;
+    public float dashSpeed = 8f;
+    public float dashLength = 0.5f;
+    public float dashCooldown = 1f;
+    public float dashInvinciblity = 0.5f;
+    private float dashCounter;
+    private float dashCoolCounter;
+
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        theCam = Camera.main;
+        activeMoveSpeed = moveSpeed;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Basic Movement
+        moveInput.x = Input.GetAxisRaw("Horizontal");
+        moveInput.y = Input.GetAxisRaw("Vertical");
+
+        moveInput.Normalize(); //Makes it so that diagonal movement isn't faster
+
+        //transform.position += new Vector3(moveInput.x * Time.deltaTime * moveSpeed, moveInput.y * Time.deltaTime * moveSpeed, 0f);
+
+        theRB.velocity = moveInput * activeMoveSpeed;
+
+        
+        Vector3 mousePos = Input.mousePosition;
+        Vector3 screenPoint = theCam.WorldToScreenPoint(transform.localPosition);
+
+        //Flip player based on mouse location
+        if(mousePos.x < screenPoint.x)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+            gunArm.localScale = new Vector3(-1f, -1f, 1f);
+        }
+        else
+        {
+            transform.localScale = Vector3.one;
+            gunArm.localScale = Vector3.one;
+        }
+
+
+        //Rotate gunArm
+        Vector2 offset = new Vector2(mousePos.x - screenPoint.x, mousePos.y - screenPoint.y);
+        float angle = Mathf.Atan2(offset.y, offset.x) * Mathf.Rad2Deg;
+
+        gunArm.rotation = Quaternion.Euler(0, 0, angle);
+
+        if(Input.GetMouseButtonDown(0))
+        {
+            Instantiate(bulletToFire, firePoint.position, firePoint.rotation);
+            shotCounter = timeBetweenShots;
+        }
+        if(Input.GetMouseButton(0))
+        {
+            shotCounter -= Time.deltaTime;
+            if(shotCounter<=0)
+            {
+                Instantiate(bulletToFire, firePoint.position, firePoint.rotation);
+                shotCounter = timeBetweenShots;
+            }
+        }
+        //DASH SECTION
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            if(dashCoolCounter <= 0 && dashCounter <=0)
+            {
+                activeMoveSpeed = dashSpeed;
+                dashCounter = dashLength;
+
+                Anim.SetTrigger("dash");
+            }
+
+        }
+        if(dashCounter > 0)
+        {
+            dashCounter -= Time.deltaTime;
+            if(dashCounter <=0)
+            {
+                activeMoveSpeed = moveSpeed;
+                dashCoolCounter = dashCooldown;
+            }
+        }
+        if(dashCoolCounter > 0)
+        {
+            dashCoolCounter -= Time.deltaTime;
+        }
+
+
+
+        if (moveInput != Vector2.zero)
+        {
+            Anim.SetBool("isMoving", true);
+        }
+        else
+        {
+            Anim.SetBool("isMoving", false);
+        }
+
+    }
+}
